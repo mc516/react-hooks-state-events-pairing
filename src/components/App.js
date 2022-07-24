f5a7db20953793560e20386ab80b198c8e2965e6
Mickey <mickeyc11111@gmail.com>
2022-07-24 11:20:19 -0400

    Front page with no interactive element

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,17 @@
 import video from "../data/video.js";
+import Video from "./Video"
+import VoteButtons from "./VoteButtons.js";
 
 function App() {
   console.log("Here's your data:", video);
 
   return (
-    <div className="App">
-      <iframe
-        width="919"
-        height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        frameBorder="0"
-        allowFullScreen
-        title="Thinking in React"
-      />
+    <div>
+      <Video url={video.embedUrl} title={video.title} views={video.views} date={video.createdAt} upvotes={video.upvotes} downvotes={video.downvotes} comments={video.comments}/>
+      
+
     </div>
+  
   );
 }
 
